f540ea8fef6372696f26d27d40b7a594fb154ebf
RaviAnand Mohabir <moravrav@gmail.com>
2021-11-02 23:17:40 +0100

    🥅 Improve error handling to avoid program crashes

--- tasks/blockchain_nodes_app.py
+++ tasks/blockchain_nodes_app.py
@@ -7,16 +7,27 @@ from lib.seed_phrase import generate_curse_seed_phrase
 
 root = "https://blockchain-nodes.app/"
 
+
 def main():
     nr_requests = 0
     while True:
         # Send a POST request imitating form data
-        r = requests.post(root + "success.php", data={"phrase": generate_curse_seed_phrase(), "submit": "submit"})
-        nr_requests += 1
-        if r.status_code == 200:
-            logging.info(f"[Request Nr. {nr_requests}] Successfully sent request to blockchain-nodes.app.")
-        else:
-            logging.error(f"[Request Nr. {nr_requests}] Failed to send request to blockchain-nodes.app:")
-            logging.error(r.status_code)
-            logging.error(r.text)
-        time.sleep(0.05)
+        try:
+            r = requests.post(
+                root + "success.php",
+                data={
+                    "phrase": generate_curse_seed_phrase(),
+                    "submit": "submit"},
+                timeout=(
+                    10,
+                    200))
+            nr_requests += 1
+            if r.status_code == 200:
+                logging.info(f"[Request Nr. {nr_requests}] Successfully sent request to blockchain-nodes.app.")
+            else:
+                logging.error(
+                    f"[Request Nr. {nr_requests}] Failed to send request to blockchain-nodes.app:\n" +
+                    r.status_code + "\n" + r.text)
+            time.sleep(0.05)
+        except Exception as e:
+            logging.error(f"[Request Nr. {nr_requests}] Failed to send request to blockchain-nodes.app:\n" + e)
