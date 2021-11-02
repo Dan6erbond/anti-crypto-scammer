812e65722d8ba4ec5373a3f4773b497a144a8d7f
RaviAnand Mohabir <moravrav@gmail.com>
2021-11-02 18:16:36 +0100

    🎉 Initialize anti-crypto-scammer project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Crypto Anti-Scammer
+
+A boilerplate codebase to deal with crypto-related scams, such as seedphrase miners.
---
+++ tests/seed_phrase_test.py
@@ -0,0 +1,19 @@
+import re
+from unittest import TestCase
+
+seed_phrase_pattern = re.compile(r'^([a-zA-Z]|\s)+$')
+
+
+class TestSeedPhrase(TestCase):
+    def test_seed_phrase(self):
+        from lib import seed_phrase
+        generated_seed_phrase = seed_phrase.generate_curse_seed_phrase()
+        self.assertEqual(len(generated_seed_phrase.split(" ")), 24)
+        self.assertTrue(seed_phrase_pattern.match(generated_seed_phrase))
+
+
+    def test_seed_phrase_length(self):
+        from lib import seed_phrase
+        generated_seed_phrase = seed_phrase.generate_curse_seed_phrase(12)
+        self.assertEqual(len(generated_seed_phrase.split(" ")), 12)
+        self.assertTrue(seed_phrase_pattern.match(generated_seed_phrase))
