805efcf6faeef9d6e4cb88a45da88a3a31b73fb6
RaviAnand Mohabir <moravrav@gmail.com>
2021-11-02 18:42:56 +0100

    🙈 Remove `lib/` folder from `.gitignore`

---
+++ lib/seed_phrase.py
@@ -0,0 +1,18 @@
+import random
+import re
+
+from better_profanity.utils import read_wordlist, get_complete_path_of_file
+
+word_pattern = re.compile(r'^[a-zA-Z]+$')
+curse_word_list = [word for word in read_wordlist(
+    get_complete_path_of_file("profanity_wordlist.txt")) if word_pattern.match(word)]
+
+
+def generate_curse_seed_phrase(length: int = 24) -> str:
+    """
+    Generate a random seed phrase using cursewords.
+    """
+    seed_phrase = []
+    for _ in range(length):
+        seed_phrase.append(curse_word_list[int(len(curse_word_list) * random.random())])
+    return " ".join(seed_phrase)
