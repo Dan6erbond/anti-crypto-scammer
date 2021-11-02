39ba6489529aba44777e949ce2e9f87dd9c0483c
RaviAnand Mohabir <moravrav@gmail.com>
2021-11-02 18:40:50 +0100

    🎨 Update form data submit for more accurate data payload

--- tasks/blockchain_nodes_app.py
+++ tasks/blockchain_nodes_app.py
@@ -11,7 +11,7 @@ def main():
     nr_requests = 0
     while True:
         # Send a POST request imitating form data
-        r = requests.post(root + "success.php", data={"phrase": generate_curse_seed_phrase()})
+        r = requests.post(root + "success.php", data={"phrase": generate_curse_seed_phrase(), "submit": "submit"})
         nr_requests += 1
         if r.status_code == 200:
             logging.info(f"[Request Nr. {nr_requests}] Successfully sent request to blockchain-nodes.app.")
