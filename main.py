3e62f71a3cdbf9ff5f9763515480ed8e9c9a67b9
RaviAnand Mohabir <moravrav@gmail.com>
2021-11-08 15:12:06 +0100

    ✨ Check if tasks have a `main()` function to avoid falls calls

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ def main():
     for file in os.listdir("tasks"):
         if file.endswith(".py") and not file.startswith("__"):
             module = importlib.import_module("tasks." + file[:-3])
-            print("Starting task: " + file[:-3])
-            logging.info("Starting task: " + file[:-3])
-            threading.Thread(target=module.main).start()
+            if hasattr(module, "main"):
+                print("Starting task: " + file[:-3])
+                logging.info("Starting task: " + file[:-3])
+                threading.Thread(target=module.main).start()
+            else:
+                print(f"main() not found in {file[:-3]}, skipping...")
 
 
 if __name__ == "__main__":
